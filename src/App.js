4bb513f15f971cc6675e9246ef285965a81d2a78
Thiago Wriel <140001164+ThiagoWriel@users.noreply.github.com>
2026-01-09 15:00:52 -0300

    arrumando a casa

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ import { CriarQuarto, CriarCliente } from "./components/Create";
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <h1>Hotler</h1>
-        <Link to="/">Dashboard</Link>
-        <Link to="/quartos">Quartos</Link>
-        <Link to="/clientes">Clientes</Link>
-        <Link to="/reservas">Reservas</Link>
-        <Link to="/financeiro">Financeiro</Link>
-      </nav>
+      <div className="page">
+        <nav>
+          <div className="principal-title">
+            <h1>Hotler</h1>
+          </div>
+          <Link to="/">Dashboard</Link>
+          <Link to="/quartos">Quartos</Link>
+          <Link to="/clientes">Clientes</Link>
+          <Link to="/reservas">Reservas</Link>
+          <Link to="/financeiro">Financeiro</Link>
+        </nav>
+      </div>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/quartos" element={<Quartos />} />
